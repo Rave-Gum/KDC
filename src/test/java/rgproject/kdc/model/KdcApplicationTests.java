d348d9fcbb98e456a8b0476274d63182c50ad2b6
jeongjun Hwang <hsk2454@naver.com>
2021-06-18 23:09:37 +0900

    feat: KDC 프로젝트 생성

---
+++ src/test/java/rgproject/kdc/model/KdcApplicationTests.java
@@ -0,0 +1,13 @@
+package rgproject.kdc.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
