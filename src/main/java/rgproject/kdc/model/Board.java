0c7b4ffc84a4e8eb59a4eabc45b7272183b25c4c
jeongjun Hwang <47655983+DeerGum@users.noreply.github.com>
2021-06-24 17:09:42 +0900

    feat: 데이터 모델 코드 작성 (#2)

--- src/main/java/rgproject/kdc/model/Board.java
+++ src/main/java/rgproject/kdc/model/Board.java
@@ -2,4 +2,37 @@ package rgproject.kdc.model;
 
 //@Entity
 public class Board {
+
+    //@Id @GeneratedValue
+    //@Column(name = "MEMBER_ID")
+    private Long id;
+    private String name;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "MEMBER_ID")
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- src/main/java/rgproject/kdc/model/Comment.java
+++ src/main/java/rgproject/kdc/model/Comment.java
@@ -1,5 +1,24 @@
 package rgproject.kdc.model;
 
+import java.util.Date;
+
 //@Entity
 public class Comment {
+
+    //@Id @GeneratedValue
+    //@Column(name = "COMMENT_ID")
+    private Long id;
+    //@Lob
+    private String content;
+    //@Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "POST_ID")
+    private Post post;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
 }
